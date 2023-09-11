0ad8bf9236c0602e8eb9ff9a77a102de721518fc
donrobin3 <121735034+donrobin3@users.noreply.github.com>
2023-09-11 11:51:00 -0500

    feature 2 completed

--- Properties/Models/Dog.cs
+++ Properties/Models/Dog.cs
@@ -6,5 +6,7 @@ public class Dog
     public string Breed { get; set; }
     public int Age { get; set; }
     public int CityId { get; set; }
-    public int WalkerId { get; set; }
+    public int? WalkerId { get; set; }
+    public Walker Walker { get; set; }
+    public City City { get; set; }
 }
--- client/src/DogList.js
+++ client/src/DogList.js
@@ -1,18 +1,22 @@
+
+
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { DogDetails } from "./DogDetails.js";
 
 export const DogList = ({ dogs }) => {
     return (
-        <div className="dog-list-container"> {/* Add class name to the div */}
-            <h2 className="dog-list-heading">List of Dogs:</h2> {/* Add class name to the heading */}
+        <div className="dog-list-container">
+            <h2 className="dog-list-heading">List of Dogs:</h2>
             <div className="dog-list">
                 {dogs.map((dog) => (
-                    <li key={dog.id} className="dog-item"> {/* Add class name to list items */}
-                        {dog.name}
+                    <li key={dog.id} className="dog-item">
+                        <Link to={`/dogdetails/${dog.id}`}> {/* Set the link to the DogDetails page */}
+                            {dog.name}
+                        </Link>
                     </li>
                 ))}
             </div>
         </div>
     );
 }
-
-
